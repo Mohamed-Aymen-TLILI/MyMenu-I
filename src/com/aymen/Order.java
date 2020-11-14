b536994197b6400c79749f3f5377d492106181e1
libra1919 <aymentli@gmail.com>
2020-11-14 23:04:36 +0100

    "tome 2"

--- src/com/aymen/Order.java
+++ src/com/aymen/Order.java
@@ -1,23 +1,45 @@
 package com.aymen;
 
+import java.util.Scanner;
+
 public class Order {
     /**
      * Display all available menus in the restaurant.
      */
     public void displayAvailableMenu() {
-        System.out.println("Choix de Menu");
-        System.out.println("1 - Poulet");
-        System.out.println("2 - Beouf");
-        System.out.println("3 - Végan");
-        System.out.println("1 - Que souhaitez-vous comme menu ?");
-
+        System.out.println("Choix menu");
+        System.out.println("1 - poulet");
+        System.out.println("2 - boeuf");
+        System.out.println("3 - végétarien");
+        System.out.println("Que souhaitez-vous comme menu ?");
     }
     /**
      * Display a selected menu.
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
-        System.out.println("Vous avez choisi le menu " + nbMenu);
-
+        switch (nbMenu) {
+            case 1:
+                System.out.println("Vous avez choisi comme menu : poulet");
+                break;
+            case 2:
+                System.out.println("Vous avez choisi comme menu : boeuf");
+                break;
+            case 3:
+                System.out.println("Vous avez choisi comme menu : végétarien");
+                break;
+            default:
+                System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+                break;
+        }
+    }
+    /**
+     * Run asking process for a menu.
+     */
+    public void runMenu() {
+        this.displayAvailableMenu();
+        Scanner sc = new Scanner(System.in);
+        int nb = sc.nextInt();
+        this.displaySelectedMenu(nb);
     }
 }
